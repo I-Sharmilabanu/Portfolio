89e922e2bef4ca2b3eafca66720e6fe3e40b2e37
I-Sharmilabanu <banu69136@gmail.com>
2026-01-10 23:06:31 +0530

    updated portfolio

--- portfolio-project/src/sections/About.jsx
+++ portfolio-project/src/sections/About.jsx
@@ -3,22 +3,23 @@ import img from "../assets/images/ChatGPT Image Jan 8, 2026, 04_35_35 PM.png"
 
 const About = () => {
   return (
-    <section className="py-7 px-6 mt-20 ">
+    <section className=" px-6 mt-20 ">
       <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-12 items-center ">
 
         {/* LEFT SIDE – IMAGE */}
+                 <h2 className="text-3xl  font-bold  hover:text-violet-500">About Me</h2>
+
         <motion.div
           initial={{ opacity: 0, x: -40 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6 }}
-          className="flex justify-center "
+          className="flex justify-end "
         >   
-         <h2 className="text-3xl  font-bold  hover:text-violet-500">About Me</h2>
 
           <img
             src={img}
             alt="Tech Stack"
-            className="w-72  rounded-2xl shadow-xl mt-14 mr-20"
+            className="w-72  rounded-2xl shadow-xl  mr-20"
           />
         </motion.div>
 
